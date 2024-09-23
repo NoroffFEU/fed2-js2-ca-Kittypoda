325a3b0277bb9f495d55d924639956aea6a9cd5f
Kittypoda <odakrolstad@gmail.com>
2024-09-23 14:37:10 +0200

    login ok

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,36 @@
-export async function onLogin(event) {}
+import { API_AUTH_LOGIN } from "../../api/constants";
+export async function onLogin(event) {
+  event.preventDefault()
+  
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const userData = {
+      email,
+      password
+    };
+
+    try {
+      const sendUserData = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+      };
+
+      const response = await fetch (API_AUTH_LOGIN, sendUserData)
+      const json = await response.json();
+      console.log(json)
+      return json;
+    } catch (error) {
+      alert('Login failed')
+      console.error('Login failed:', error);
+    }
+
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('userName',JSON.stringify(json.data.name))
+  }
+
+
