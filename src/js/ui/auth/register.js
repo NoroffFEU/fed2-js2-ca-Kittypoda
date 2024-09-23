3aacf4eda066a01927ee9eadbb2d52104fbb99bc
Kittypoda <odakrolstad@gmail.com>
2024-09-23 14:12:37 +0200

    register ok

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,1 +1,50 @@
-export async function onRegister(event) {}
+import { API_AUTH_REGISTER } from "../../api/constants";
+export async function onRegister(event) {
+  event.preventDefault()
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const userData = {
+      name,
+      email,
+      password
+    };
+console.log(userData)
+    try {
+      const sendUserData = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+      };
+
+      const response = await fetch (API_AUTH_REGISTER, sendUserData)
+      const json = await response.json();
+      console.log(json)
+      return json;
+    } catch (error) {
+      alert('Registration failed')
+      console.error('Registration failed:', error);
+    }
+   
+    }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// her skal funksjonen være
