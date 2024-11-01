38a5a63f4900189f5210db88c80eb61755289603
Kittypoda <odakrolstad@gmail.com>
2024-11-01 11:33:47 +0100

    path name fix

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -3,32 +3,32 @@
 // You may change the behaviour or approach of this file if you choose
 export default async function router(pathname = window.location.pathname) {
   switch (pathname) {
-    case "/":
+    case "/fed2-js2-ca-Kittypoda/":
       await import("./views/home.js");
       await import("./views/posts.js")
       break;
-    case "/auth/":
+    case "/fed2-js2-ca-Kittypoda/auth/":
       await import("./views/auth.js");
       break;
-    case "/auth/login/":
+    case "/fed2-js2-ca-Kittypoda/auth/login/":
       await import("./views/login.js");
       break;
-    case "/auth/register/":
+    case "/fed2-js2-ca-Kittypoda/auth/register/":
       await import("./views/register.js");
       break;
-    case "/post/":
+    case "/fed2-js2-ca-Kittypoda/post/":
       await import("./views/post.js");
       break;
-    case "/post/edit/":
+    case "/fed2-js2-ca-Kittypoda/post/edit/":
       await import("./views/postEdit.js");
       break;
-    case "/post/create/":
+    case "/fed2-js2-ca-Kittypoda/post/create/":
       await import("./views/postCreate.js");
       break;
-    case "/profile/":
+    case "/fed2-js2-ca-Kittypoda/profile/":
       await import("./views/profile.js");
       break;
-    case "/":
+    case "/fed2-js2-ca-Kittypoda/":
       await import("./views/posts.js");
       break;
       default:
