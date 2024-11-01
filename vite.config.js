171dc7e95b8b70a435c10e047c08e428f74b032b
Kittypoda <odakrolstad@gmail.com>
2024-11-01 12:16:41 +0100

    base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   appType: "mpa",
-  base: "/fed2-js2-ca-Kittypoda/",
+  base: "",
   build: {
     target: "esnext",
     outDir: "./dist",
