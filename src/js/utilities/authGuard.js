3c7cce6c93d5c84330bc502e17ce724aa34548bf
Kittypoda <odakrolstad@gmail.com>
2024-11-01 12:00:38 +0100

    h

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -4,6 +4,6 @@ export function authGuard() {
 
   if (!token) {
     alert("You must be logged in to view this page");
-    window.location.href = "./fed2-js2-ca-Kittypoda/auth/login/";  
+    window.location.href = "/fed2-js2-ca-Kittypoda/auth/login/";  
   }
 }
